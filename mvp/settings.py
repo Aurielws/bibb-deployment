21ec6f0c40ad72ebfdcff47370af0075c83cc956
AURIEL WRIGHT <AURIELWRIGHT@COLLEGE.HARVARD.EDU>
2020-12-02 09:18:27 -0500

    fix static root

--- mvp/settings.py
+++ mvp/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 # Specify static files location
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-STATIC_ROOT = (os.path.join(BASE_DIR, "staticfiles"), )
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
