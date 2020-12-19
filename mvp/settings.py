e5fa21e01ffab772ca2b83e1577ce9cf67c964cb
HelixAchaos <40044108+HelixAchaos@users.noreply.github.com>
2020-12-18 19:03:52 -0500

    Update settings.py

--- mvp/settings.py
+++ mvp/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'cs96.test@gmail.com'
-EMAIL_HOST_PASSWORD = 'cs96pass!'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 
